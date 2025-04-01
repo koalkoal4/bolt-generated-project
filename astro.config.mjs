cd0cac1096d4017e63d1d508d4bf988d59254143
koalkoal4 <keremtekin2307@gmail.com>
2025-04-01 17:33:33 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'; // Import the sitemap integration
 
 export default defineConfig({
-  site: 'https://trainedtails.com',
-  integrations: [tailwind()]
+  site: 'https://trainedtails.com', // This is required for the sitemap
+  integrations: [
+    tailwind(),
+    sitemap() // Add the sitemap integration
+  ]
 });
