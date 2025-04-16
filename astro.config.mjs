ba1c3937629f091dcc93153902b6ad244436b7a4
koalkoal4 <keremtekin2307@gmail.com>
2025-04-16 16:19:25 +0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://trainedtails.com',
+  trailingSlash: 'never', // Ensure no trailing slashes in URLs
   integrations: [tailwind(), sitemap()]
 });
