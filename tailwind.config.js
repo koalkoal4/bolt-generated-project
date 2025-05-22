c3b0a230f9545010c45dc811fed45127b2dc9406
koalkoal4 <keremtekin2307@gmail.com>
2025-05-22 16:23:20 +0300

    new landing pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
           50: '#f8fafc',
           800: '#1e293b',
           900: '#0f172a'
-        }
+         },
+        'trainedtails-orange': '#FE9D00', // Updated orange color
+        'trainedtails-blue-light': '#A4C5E4', // Updated light blue for the pet care section
+        'trainedtails-blue-dark': '#1A365D', // Dark blue for the video section
+        'trainedtails-dark-footer': '#E2E2E2', // Updated color for the footer
+        'trainedtails-blue-form': '#C3ECFF', // New bluish color for the form box
+        'trainedtails-blue-links': '#4A90E2' // New color for footer links and copyright text
       }
     }
   },
